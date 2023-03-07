8be699e3c3832bac1621bdef53a9366c45adfa25
Emma Olsson <emma.olsson22@learnet.se>
2023-03-07 09:14:41 +0100

    edit

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectToDB() {
   try {
     await mongoose.connect(process.env.DB_URL);
-    console.log('Connected to database ');
+    console.log('Connected to database');
   } catch (err) {
     console.log(err);
   }
