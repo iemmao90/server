65275845e6db15b014a529ee747740d8a4893389
Emma Olsson <emma.olsson22@learnet.se>
2023-05-15 00:05:07 +0200

    rwar

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -4,26 +4,26 @@ const User = require('../models/user');
 async function requireAuth(req, res, next) {
   // Read token of cookies
   const token = req.cookies.Authorization;
-  try {
-    // Decode the token
-    const decoded = jwt.verify(token, process.env.SECRET);
+  // try {
+  //   // Decode the token
+  //   const decoded = jwt.verify(token, process.env.SECRET);
 
-    // Check expiration
+  //   // Check expiration
 
-    if (Date.now() > decoded.exp) return res.sendStatus(401);
+  //   if (Date.now() > decoded.exp) return res.sendStatus(401);
 
-    // Find user using the decoded sub
-    const user = await User.findById(decoded.sub);
-    if (!user) return res.sendStatus(401);
+  //   // Find user using the decoded sub
+  //   const user = await User.findById(decoded.sub);
+  //   if (!user) return res.sendStatus(401);
 
-    // Attach user to req
-    req.user = user;
+  //   // Attach user to req
+  //   req.user = user;
 
-    // Continue on
-    next();
-  } catch (err) {
-    return res.sendStatus(401);
-  }
+  //   // Continue on
+  //   next();
+  // } catch (err) {
+  //   return res.sendStatus(401);
+  // }
 }
 
 module.exports = requireAuth;
